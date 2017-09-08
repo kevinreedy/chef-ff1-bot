2b41c159fec34abfaae092d6769f155ac359cf79
Kevin Reedy <kevinreedy@gmail.com>
2017-09-08 16:55:57 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'lita'
+gem 'lita-dotenv'
+
+# Use Slack adapter
+gem 'lita-slack'
+
+# Add handlers to give Lita new functionality.
+gem 'lita-espn-fantasy-football'
---
+++ lita_config.rb
@@ -0,0 +1,22 @@
+Lita.configure do |config|
+  # The name your robot will use.
+  config.robot.name = 'chef-ff3-bot'
+
+  # The locale code for the language to use.
+  # config.robot.locale = :en
+
+  config.robot.log_level = :info
+
+  # config.robot.admins = ["1", "2"]
+
+  config.robot.adapter = :slack
+  config.adapters.slack.token = ENV['SLACK_TOKEN']
+
+  config.redis[:host] = ENV['REDIS_HOST'] || '127.0.0.1'
+  config.redis[:port] = ENV['REDIS_PORT'] || 6379
+
+  config.handlers.espn_fantasy_football.league_id = ENV['ESPN_LEAGUE_ID']
+  config.handlers.espn_fantasy_football.season_id = '2017'
+  config.handlers.espn_fantasy_football.activity_room = 'chef-ff-3'
+  config.handlers.espn_fantasy_football.activity_interval = 900
+end
